30f62242506fef67b96bc272a03d384a9900ed23
ericsato77 <167514173+ericsato77@users.noreply.github.com>
2024-05-29 20:52:13 +0200

    Add files via upload

---
+++ Projects/Lab-9/Derived.cpp
@@ -0,0 +1,7 @@
+#include "Derived.h"
+Derived::Derived()
+
+{
+
+a = 0;
+}
---
+++ Projects/Lab-9/Polygon.cpp
@@ -0,0 +1,12 @@
+#include "Polygon.h"
+#include <iostream>
+using namespace std;
+void Polygon::SetValues(int width, int height)
+{
+mWidth = width;
+mHeight = height;
+} 
+void Polygon::PrintArea()
+{
+cout << this->Area() << '\n';
+}
